b4c6307fcfb628a69f9cc6f2988df9d9110e2834
misun <mcoding777@naver.com>
2022-07-25 22:54:42 +0900

    chore: 로고 이미지 변경

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
-import './home_screen.dart';
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
