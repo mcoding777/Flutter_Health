8da3059cafb25ba4b436a334119dd047a19d4655
misun <mcoding777@naver.com>
2022-08-21 22:55:23 +0900

    feat: 운동 세트 모델 추가

---
+++ lib/models/exercise_set.dart
@@ -0,0 +1,13 @@
+class ExerciseSet {
+  late int set;
+  late String before; // 이전에 저장한 기록
+  late int kg;
+  late int rep; // 이번 운동 횟수
+
+  ExerciseSet({
+    required this.set,
+    required this.before,
+    required this.kg,
+    required this.rep,
+  });
+}
