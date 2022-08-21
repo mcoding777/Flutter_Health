5356bd7b13d25e66df7bc7b93284fc5b8120ac36
misun <mcoding777@naver.com>
2022-08-21 22:22:58 +0900

    refactor: day toggle button 구현

--- lib/components/button/day_toggle_button.dart
+++ lib/components/button/day_toggle_button.dart
@@ -10,11 +10,19 @@ class DayToggleButton extends StatefulWidget {
 
 class DayToggleButtonState extends State<DayToggleButton>
     with SingleTickerProviderStateMixin {
+  // AnimationController Timer (1개의 애니메이션)
   late TabController _tabController;
+  // _tabController.index로 현재 선택된 index에 접근 가능
+
+  List<Button> buttons = <Button>[
+    Button(widget: const Tab(text: "연도별"), isSelected: false),
+    Button(widget: const Tab(text: "월별"), isSelected: false),
+    Button(widget: const Tab(text: "일별"), isSelected: true),
+  ];
 
   @override
   void initState() {
-    _tabController = TabController(length: 2, vsync: this);
+    _tabController = TabController(length: 3, vsync: this);
     super.initState();
   }
 
@@ -29,12 +37,12 @@ class DayToggleButtonState extends State<DayToggleButton>
     return LayoutBuilder(
       builder: (context, constraints) {
         return Container(
-          width: (constraints.maxWidth / 4) - (5 / 4),
-          height: 45,
+          width: (constraints.maxWidth / 2),
+          height: 35,
           decoration: BoxDecoration(
-            color: Colors.grey[300],
+            color: Colors.grey[500],
             borderRadius: BorderRadius.circular(
-              25.0,
+              10.0,
             ),
           ),
           child: TabBar(
@@ -42,23 +50,22 @@ class DayToggleButtonState extends State<DayToggleButton>
             // give the indicator a decoration (color and border radius)
             indicator: BoxDecoration(
               borderRadius: BorderRadius.circular(
-                25.0,
+                10.0,
               ),
-              color: Colors.green,
+              border: Border.all(
+                color: Colors.grey.shade500,
+                width: 5,
+              ),
+              color: Colors.white,
             ),
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.black,
-            tabs: const [
-              // first tab [you can add an icon using the icon property]
-              Tab(
-                text: 'Place Bid',
-              ),
-
-              // second tab [you can add an icon using the icon property]
-              Tab(
-                text: 'Buy Now',
-              ),
-            ],
+            labelStyle:
+                const TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+            labelColor: Colors.black,
+            unselectedLabelStyle: const TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+            tabs: buttons.map((e) => e.widget).toList(),
+            labelPadding: const EdgeInsets.all(0), // 글씨 흐릿해지는 현상 제거
           ),
         );
       },
